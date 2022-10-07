fe950988c3c010b3f6f8492960a4d4e22c9db898
feath3rz <96647951+feath3rz@users.noreply.github.com>
2022-10-07 01:41:29 -1000

    Create Day21Part2.py

---
+++ 2020-Code-Python/Day21Part2.py
@@ -0,0 +1,22 @@
+# -----------------------------------------------------------------------------------
+# --- Part Two ---
+# Now that you've isolated the inert ingredients, you should have enough 
+# information to figure out which ingredient contains which allergen.
+#
+# In the above example:
+#
+#   - mxmxvkd contains dairy.
+#   - sqjhc contains fish.
+#   - fvjkl contains soy.
+#
+# Arrange the ingredients alphabetically by their allergen and separate them 
+# by commas to produce your canonical dangerous ingredient list. (There 
+# should not be any spaces in your canonical dangerous ingredient list.) In 
+# the above example, this would be mxmxvkd,sqjhc,fvjkl.
+#
+# Time to stock your raft with supplies. What is your canonical dangerous
+# ingredient list?
+# -----------------------------------------------------------------------------------
+# Just like Day20, this is lost since I did it in Excel. I'll work to do
+# Python instead and get this working.
+# -----------------------------------------------------------------------------------
